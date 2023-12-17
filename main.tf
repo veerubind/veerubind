57fb740183ee3b6340014cf3fad305b5bde3e8f4
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-17 11:37:01 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "ASIA"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
+  }
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
+
+
+
+
 resource "google_storage_bucket" "tf-bucket" {
   name     = "tf-state-bucket-22"
   location = "asia-south1"
