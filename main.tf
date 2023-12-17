c6d4b651abead9d5bc654e4fd896c0082fe11e48
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-17 11:42:52 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,6 @@ resource "google_storage_bucket" "default" {
   versioning {
     enabled = true
   }
-  encryption {
-    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
-  }
-  depends_on = [
-    google_project_iam_member.default
-  ]
 }
 
 
