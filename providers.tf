e393c3bd98ab740670e93f98d1d59f4a9c4b555c
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-17 11:50:32 +0530

    Update providers.tf
    m

--- providers.tf
+++ providers.tf
@@ -2,3 +2,10 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-bucket-22"
+    prefix = "terraform/state"
+  }
+}
+
