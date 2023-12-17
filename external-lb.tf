11464a78c6bea0b8a7c8776394c48b9079830ed3
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-17 17:21:22 +0530

    Update external-lb.tf
    wewq

--- external-lb.tf
+++ external-lb.tf
@@ -1,5 +1,5 @@
-module "https-load-balancer" {
-  source  = "gruntwork-io/load-balancer/google//modules/http-load-balancer"
-  version = "0.2.0"
-  # insert the 3 required variables here
-}
+#module "https-load-balancer" {
+#  source  = "gruntwork-io/load-balancer/google//modules/http-load-balancer"
+#  version = "0.2.0"
+#  # insert the 3 required variables here
+#}
