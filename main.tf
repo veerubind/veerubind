c9c06f289c18b08b9ffc4eb0b8e692d8e7dfecb3
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-17 10:35:32 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+resource "google_storage_bucket" "tf-bucket" {
+  name     = "tf-state-bucket-22"
+  location = "asia-south1"
+}
 resource "google_storage_bucket" "bucket" {
   name     = "veer-test-bucket-22"
   location = "asia-south1"
