0acdfa40a77739c59cd1571eb14f4994f6561584
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-17 10:54:34 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ resource "google_storage_bucket" "bucket" {
   name     = "veer-test-bucket-22"
   location = "asia-south1"
 }
+
+resource "google_storage_bucket_object" myimage" {
+  name = "hello-file"
+  bucket = "veer-test-bucket-22"
+  source = "hello.txt"
+}
