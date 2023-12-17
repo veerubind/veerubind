72c541898818502488eb169c835af103e1430549
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-17 11:47:14 +0530

    tfstate Update main.tf
    tfstate Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "default" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
-  location      = "ASIA"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
-
-
-
-
 resource "google_storage_bucket" "tf-bucket" {
   name     = "tf-state-bucket-22"
   location = "asia-south1"
