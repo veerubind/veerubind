5371390efc755217ecaac0227bdb864a26c51d2a
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-19 11:10:38 +0530

    Update variables.tf
    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,10 +13,10 @@ variable "region" {
 #  type        = string
 # }
 
-variable "zone" {
-  description = "The availability zone to create the sample compute instances in. Must within the region specified in 'var.region'"
-  type        = string
-}
+# variable "zone" {
+#  description = "The availability zone to create the sample compute instances in. Must within the region specified in 'var.region'"
+#  type        = string
+# }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
