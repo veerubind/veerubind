8684fa8060ce9bf512807bca2121f67f31aefecd
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-19 14:51:53 +0530

    Update external-lb.tf
    Update external-lb.tf

--- external-lb.tf
+++ external-lb.tf
@@ -2,29 +2,29 @@
 resource "google_compute_global_network_endpoint" "default" {
  global_network_endpoint_group = google_compute_global_network_endpoint_group.default.name
  fqdn                          = "www.example.com"
- port                          = 90
+ port                          = 443
 }
 
 # Add network endpoint to network endpoint group
 resource "google_compute_global_network_endpoint_group" "default" {
- name                  = "example-neg"
- default_port          = "90"
+ name                  = "veer22-neg"
+ default_port          = "443"
  network_endpoint_type = "INTERNET_FQDN_PORT"
 }
 
-/*
+
 # Since this is an external LB it needs a SSL certificate
 resource "google_compute_managed_ssl_certificate" "default" {
- name = "example-cert"
+ name = "dev22-cert"
 
  managed {
-   domains = ["example.com"]
+   domains = ["www,example.com"]
  }
 }
 
 # Forwarding rule
-resource "google_compute_global_forwarding_rule" "example" {
- name                  = "example-forwarding-rule"
+resource "google_compute_global_forwarding_rule" "default" {
+ name                  = "veer22-forwarding-rule"
  provider              = google-beta
  port_range            = "443"
  target                = google_compute_target_https_proxy.default.self_link
@@ -34,25 +34,25 @@ resource "google_compute_global_forwarding_rule" "example" {
 # Public IP address
 resource "google_compute_global_address" "default" {
  provider      = google-beta
- name          = "example-address"
+ name          = "dev22-address"
  ip_version    = "IPV4"
 }
 
 # HTTPS proxy 
 resource "google_compute_target_https_proxy" "default" {
- name             = "example-proxy"
+ name             = "veer22-proxy"
  url_map          = google_compute_url_map.default.id
  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
 }
 
 # URL Map
 resource "google_compute_url_map" "default" {
- name        = "example-urlmap"
+ name        = "veer-ext-lb"
  description = "Example url map"
 
  default_service = google_compute_backend_service.default.id
  }
-*/
+
 
 # Make NEG the backend
 resource "google_compute_backend_service" "veer-neg-backend" {
