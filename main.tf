9647942a9c701eb5df7c704ee09dc628e801469a
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-25 13:26:49 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,28 @@ resource "google_storage_bucket_object" "myimage" {
   depends_on = [google_storage_bucket.test-bucket]
 }
 
+#VPC creation
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+data "google_compute_zones" "dev22-zone" {
+  region  = var.region
+  project = var.project_id
+}
+
+locals {
+  type   = ["public", "private"]
+  zones = data.google_compute_zones.dev22-zone.names
+}
+
+
+# VPC
+resource "google_compute_network" "dev22-vpc" {
+  name                            = "${var.vpcname}-vpc"
+  delete_default_routes_on_create = false
+  auto_create_subnetworks         = true
+  routing_mode                    = "REGIONAL"
+}
