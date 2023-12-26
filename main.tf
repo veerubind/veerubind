cbde6b171f13ed3f7229e62b5df3f0a397389ab3
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-26 19:53:30 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -63,18 +63,18 @@ resource "google_compute_router_nat" "nat-route" {
 }
 */
 
-resource "google_compute_address" "endpoint-psc-ip" {
+resource "google_compute_address" "endpoint-psc-ip1" {
   address_type = "INTERNAL"
-  name = "endpoint-psc-ip"
+  name = "endpoint-psc-ip1"
   purpose = "private endpoint terminal"
   region = "var.region"
-  subnetwork = "endpoint-subnet"
+  subnetwork = "endpoint-subnet1"
 }
 
 
 
 resource "google_compute_forwarding_rule" "dev22-psc-endpoint" {
-  ip_address = google_compute_address.endpoint-psc-ip.self_link
+  ip_address = google_compute_address.endpoint-psc-ip1.self_link
   name = "dev22-psc-endpoint"
   network = "endpoint-vpc"
   region = "var.region"
