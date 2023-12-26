5dbd8e47459782aef82f79b91fcd9868c1a3557d
veerubind <42239684+veerubind@users.noreply.github.com>
2023-12-26 16:01:51 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,8 @@ resource "google_compute_address" "endpoint-psc-ip" {
 
 
 resource "google_compute_forwarding_rule" "dev22-psc-endpoint" {
-  ip_address = 10.30.30.2
+  subnet_id = endpoint-subnet.id
+  private_ips = 10.30.30.2
   name = "dev22-psc-endpoint"
   network = "endpoint-vpc"
   region = "var.region"
