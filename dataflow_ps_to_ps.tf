01b85e0cacd6ffb4e95e438e9d84f8731a17d156
veerubind <42239684+veerubind@users.noreply.github.com>
2024-02-18 07:41:38 +0530

    Create dataflow_ps_to_ps.tf

---
+++ dataflow_ps_to_ps.tf
@@ -0,0 +1,14 @@
+resource "google_dataflow_job" "cloud_pubsub_to_cloud_pubsub" {
+
+  provider          = google-beta
+  template_gcs_path = "gs://dataflow-templates-${var.region}/latest/Cloud_PubSub_to_Cloud_PubSub"
+  name              = "cloud-pubsub-to-cloud-pubsub"
+  region            = var.region
+  temp_gcs_location = "gs://veer-dataflow-temp/temp"
+  parameters        = {
+    inputSubscription = "projects/mydev-22/subscriptions/network-sub-push-to-secmon"
+    outputTopic = "projects/mydev-22/topics/secmon-logs"
+    filterKey = "logname"
+    filterValue = "projects/mydev-22/logs/cloudaudit.googleapis.com%2Fdata_access"
+  }
+}
