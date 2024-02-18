e95abf73dd5426aba43b2ea6772fd1e02e833f78
veerubind <42239684+veerubind@users.noreply.github.com>
2024-02-18 15:24:06 +0530

    Update dataflow_ps_to_ps.tf

--- dataflow_ps_to_ps.tf
+++ dataflow_ps_to_ps.tf
@@ -3,8 +3,7 @@ resource "google_dataflow_job" "cloud_pubsub_to_cloud_pubsub" {
   provider              = google-beta
   template_gcs_path     = "gs://dataflow-templates-${var.region}/latest/Cloud_PubSub_to_Cloud_PubSub"
   name                  = "cloud-pubsub-to-cloud-pubsub"
-#  region                =  "europe-west3"
-  location              = "europe-west3-a"
+  region                =  "europe-west3"
   subnetwork            = "regions/europe-west3/subnetworks/dataflow-subnet-ew3"
   service_account_email  = "dataflow-sa@mydev-22.iam.gserviceaccount.com"
   temp_gcs_location     = "gs://veer-dataflow-temp/temp"
