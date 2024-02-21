e500170c42ae2d8b850846347f71d72beea6cf48
veerubind <42239684+veerubind@users.noreply.github.com>
2024-02-21 07:01:59 +0530

    Update logging_bucket.tf

--- logging_bucket.tf
+++ logging_bucket.tf
@@ -1,4 +1,4 @@
-resource "google_logging_project_bucket_config" "basic" {
+resource "google_logging_project_bucket" "logging_bucket" {
     project    = var.project_id
     location  = "global"
     retention_days = 30
