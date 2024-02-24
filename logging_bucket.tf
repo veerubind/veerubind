af9e01522a3a46952ae916bff25753340ceec91d
veerubind <42239684+veerubind@users.noreply.github.com>
2024-02-24 23:26:00 +0530

    Update logging_bucket.tf

--- logging_bucket.tf
+++ logging_bucket.tf
@@ -1,6 +1,8 @@
+/*
 resource "google_logging_project_bucket" "logging_bucket" {
     project    = var.project_id
     location  = "global"
     retention_days = 30
     bucket_id = "veer-logging"
 }
+*/
