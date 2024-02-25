32ddd36f4dc1d1c6dc2e42ebcbb3c67d1fdf555b
veerubind <42239684+veerubind@users.noreply.github.com>
2024-02-25 22:27:11 +0530

    Update psc-consumer-ew3.tf

--- psc-consumer-ew3.tf
+++ psc-consumer-ew3.tf
@@ -39,7 +39,7 @@ resource "google_compute_region_target_http_proxy" "psc-ep-target-ew3" {
   url_map = google_compute_region_url_map.psc-ep-url-map-ew3.id
 }
 
-/*
+
 resource "google_compute_subnetwork" "proxy_subnet-ew3" {
   name          = "proxy-subnet-ew3-ew3"
   ip_cidr_range = "10.60.60.0/26"
@@ -48,7 +48,7 @@ resource "google_compute_subnetwork" "proxy_subnet-ew3" {
   role          = "ACTIVE"
   network       = "endpoint-vpc-ew3"
 }
-*/
+
 
 resource "google_compute_forwarding_rule" "psc-ep-front-url-ew3" {
   ip_address = google_compute_address.endpoint-psc-ip-ew3.self_link
@@ -57,7 +57,7 @@ resource "google_compute_forwarding_rule" "psc-ep-front-url-ew3" {
   subnetwork = "endpoint-subnet-ew3"
   port_range = "80"
   region = var.region-ew3
-  # depends_on = [google_compute_subnetwork.proxy_subnet]
+  depends_on = [google_compute_subnetwork.proxy_subnet]
   network_tier = "PREMIUM"
   load_balancing_scheme = "INTERNAL_MANAGED"
   target = google_compute_region_target_http_proxy.psc-ep-target-ew3.id
